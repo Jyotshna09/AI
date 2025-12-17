e2fce7331953c8c1a1f6380cb1421b96caf02139
Jyotshna09 <singamsettyjyotshna@gmail.com>
2025-12-17 13:38:12 +0530

    Add files via upload

---
+++ dfs.py
@@ -0,0 +1,19 @@
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': [],
+    'F': []
+}
+
+def dfs(graph, node, visited):
+    if node not in visited:
+        print(node, end=" ")
+        visited.add(node)
+        for neighbor in graph[node]:
+            dfs(graph, neighbor, visited)
+
+print("DFS Traversal:")
+dfs(graph, 'A', set())
